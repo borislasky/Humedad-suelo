78c7b24eb021a4725c648c22ae52a00a74ca6cd0
Boris Martínez <borislasky@gmail.com>
2019-08-05 19:18:06 +0200

    Actualización

--- calibra/calibra.ino
+++ calibra/calibra.ino
@@ -3,14 +3,24 @@
  * Està conectado a la entrada A0
  */
 
-// YD-38 sensor de humedad
-byte humidity_sensor_pin = A0;
+// YL-38 sensor de humedad
+byte SensorSalidaAnalogica = A0;
+byte AlimentacionSensor    = 5;      //D1
 
 #define NUM_MUESTRAS 100
 unsigned long i = 0, valor, acum = 0, maxi = 0, mini = 10000;
 
+unsigned int leeSensor(){
+  digitalWrite(AlimentacionSensor, HIGH);
+  delay(100); // Espero un poquito
+  valor = analogRead(SensorSalidaAnalogica);
+  digitalWrite(AlimentacionSensor, LOW);
+  return valor;
+}
+
 void setup() {
   Serial.begin(115200);
+  pinMode(AlimentacionSensor, OUTPUT);
 }
 
 void loop() {
@@ -18,11 +28,11 @@ void loop() {
     Serial.printf("Valor medio: %3d. Máx: %ld. Mín: %ld %\n", acum/i, maxi, mini);
     acum = 0; maxi = 0; mini = 10000; i = 0;
   }
-  valor = analogRead(humidity_sensor_pin);
+  valor = leeSensor();
   acum += valor;
   if(valor > maxi) maxi = valor;
   if(valor < mini) mini = valor;
   i++;
-  //Serial.printf("Muestra %03ld: %3ld - %3ld\n" , i, valor, acum/i);
-  delay(500);
+  Serial.printf("Muestra %03ld: %ld\n" , i, valor);
+  delay(400);
 }
